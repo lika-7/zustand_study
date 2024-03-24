99627cadb9199245cd7a36d4afc736253c6259e7
lika-7 <dhwhdtn93@gmail.com>
2024-03-24 22:32:03 +0900

    1. store 설정하기 더 깍끔한 코드 형식, 2. devtools 미들웨어 사용

--- src/store/useCounterStore.js
+++ src/store/useCounterStore.js
@@ -1,15 +1,15 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { persist, devtools } from "zustand/middleware";
 
 
-export const useCounterStore = create(
-    persist(
-        (set) => ({
-            count: 1,
-            increment: () =>set((state)=> ({count: state.count + 1})),
-            reset: () => set((state)=> ({count: 1})),
-            setNumber: (number) =>set({count: number})
-        }),
-        {name: 'counter'}
-    )
-)
+let counterStore = (set) => ({
+    count: 1,
+    increment: () =>set((state)=> ({count: state.count + 1})),
+    reset: () => set((state)=> ({count: 1})),
+    setNumber: (number) =>set({count: number})
+})    
+
+counterStore = devtools(counterStore);
+counterStore = persist(counterStore, { name: 'counter' });
+
+export const useCounterStore = create(counterStore);
--- src/store/useTodoStore.js
+++ src/store/useTodoStore.js
@@ -1,43 +1,43 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { persist, devtools } from "zustand/middleware";
 
-export const useTodoStore = create(
-    persist(
-        (set)=>({
-            todos: [],
-            addTodo: (todoText) => set((state) => ({
-                todos: [
-                    ...state.todos,
-                    {
-                        text: todoText,
-                        id: getId(),
-                        isCompleted: false
-                    }
-                ]
-            })),
-        
-            deleteTodo: (todoId) => set((state) => ({
-                todos: state.todos.filter((todo) => todo.id !== todoId)
-            })),
-        
-            completeTodo: (todoId) => set((state) => ({
-                todos: state.todos.map((todo)=> {
-                    if(todo.id ===todoId){
-                        return{
-                            ...todo,
-                            isCompleted: true
-                        }
-                    }
-                    return todo;
-                })
-            }))
-        }),
-        {
-            name: 'todo',
-            getStorage: () => sessionStorage
-        }        
-    )
-)
+let todoStore = (set)=>({
+    todos: [],
+    addTodo: (todoText) => set((state) => ({
+        todos: [
+            ...state.todos,
+            {
+                text: todoText,
+                id: getId(),
+                isCompleted: false
+            }
+        ]
+    })),
+
+    deleteTodo: (todoId) => set((state) => ({
+        todos: state.todos.filter((todo) => todo.id !== todoId)
+    })),
+
+    completeTodo: (todoId) => set((state) => ({
+        todos: state.todos.map((todo)=> {
+            if(todo.id ===todoId){
+                return{
+                    ...todo,
+                    isCompleted: true
+                }
+            }
+            return todo;
+        })
+    }))
+})
+
+todoStore = devtools(todoStore);
+todoStore = persist(todoStore, {
+    name: 'todo',
+    getStorage: () => sessionStorage
+});
+
+export const useTodoStore = create(todoStore)
 
 let id = 0;
 function getId() {
